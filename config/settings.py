9e44507a8896fe21e5881280832e5572c1355501
saparbayev-azizbek-01 <saparbayev.azizbek12@gmail.com>
2025-08-21 19:09:24 +0500

    some changes

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-betyarh_t*tna42k(+w%f2b&x!tg7bzh2p3=_axtvm&t8vpjxg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['mlzone.pythonanywhere.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['mlzone.pythonanywhere.com', '127.0.0.1', 'localhost', 'ml-zone.onrender.com']
 
 
 # Application definition
