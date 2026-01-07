4c8627d62d6e9cd379d83209048bda3f7fa25cad
saparbayev-azizbek-12 <saparbayev.azizbek12@gmail.com>
2026-01-07 10:26:16 +0500

    allowed hosts was changed

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-betyarh_t*tna42k(+w%f2b&x!tg7bzh2p3=_axtvm&t8vpjxg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['mlzone.pythonanywhere.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['mlzone.pythonanywhere.com', '127.0.0.1', 'localhost', 'onrender.com']
 
 
 # Application definition
